76c128c35b89767b6f27b3bf2db0f3c3d67e4a20
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-10-25 20:27:58 +0300

    feat: FormFieldWrapperSPFieldChoice - added public property isValueCustom, added implementation of addEventListener, added public field choiceType, added interface SPFieldChoiceChoiceType; value getter and setter refactored with using of SPFieldChoiceChoiceType

--- src/FormFieldWrapper/WrappersByTypes/FormFieldWrapperSPFieldChoice.ts
+++ src/FormFieldWrapper/WrappersByTypes/FormFieldWrapperSPFieldChoice.ts
@@ -1,27 +1,51 @@
 ﻿import { FormFieldWrapper } from "../FormFieldWrapper";
 
+enum SPFieldChoiceChoiceType {
+	ChoiceRadio,
+	FillInTable,
+	DropDownButton
+}
+
 export class FormFieldWrapperSPFieldChoice extends FormFieldWrapper {
+	private choiceType: SPFieldChoiceChoiceType;
 	constructor(fieldTitle: string) {
 		super(fieldTitle);
+		this.choiceType = this.getChoiceType();
 	}
 
 	public get value(): string {
 		if (window.location.href.match(/DispForm\.aspx/i))
 			return this.getValueDispForm();
 		let value;
-		if (this.fieldElement.querySelector("table[id$=ChoiceRadioTable]"))
+		if (this.choiceType == SPFieldChoiceChoiceType.ChoiceRadio)
 			value = this._getValueFromRadioButton();
-		else
-			value = this._getValueFromDropDown();
+		if (this.choiceType == SPFieldChoiceChoiceType.FillInTable)
+			value = this._getValuesFromDropDownPlusFillIn();
+		if (this.choiceType == SPFieldChoiceChoiceType.DropDownButton)
+			value = this.getValue();
 		return value;
 	}
 
 	public set value(value) {
-		if (this.fieldElement.querySelector("table[id$=ChoiceRadioTable]") || !this.fieldElement.querySelector("table[id$=FillInTable]"))
+		if (this.choiceType == SPFieldChoiceChoiceType.ChoiceRadio || this.choiceType == SPFieldChoiceChoiceType.DropDownButton )
 			super.setValue(value);
-		else
+		if (this.choiceType == SPFieldChoiceChoiceType.FillInTable)
 			this._setValueOnDropDownPlusFillIn(value);
 			
+	}
+
+	public get isValueCustom(): boolean {
+		if (this.choiceType != SPFieldChoiceChoiceType.FillInTable)
+			return false;
+		let inputRadios = this.fieldElement.querySelectorAll("input[type='radio']");
+		let checkedRadio = Array.prototype.slice.call(inputRadios)
+			.filter((i) => i.checked);
+		let checkedRadioValue = checkedRadio[0].value;
+		let selector;
+		if (checkedRadioValue == "FillInButton")
+			return true;
+		else
+			return false;
 	}
 
 	public enable(): void {
@@ -32,6 +56,20 @@ export class FormFieldWrapperSPFieldChoice extends FormFieldWrapper {
 	public disable(): void {
 		super.disable("");
 		super.disableWithStyle();
+	}
+
+	private getChoiceType(): SPFieldChoiceChoiceType {
+		let choiceType;
+		let elementToQuery: any = (window.location.href.match(/DispForm\.aspx/i)) ? 
+			this.fieldElement.parentNode :
+			this.fieldElement;
+		if (elementToQuery.querySelector("table[id$=ChoiceRadioTable]"))
+			choiceType = SPFieldChoiceChoiceType.ChoiceRadio;
+		else if (elementToQuery.querySelector("table[id$=FillInTable]"))
+			choiceType = SPFieldChoiceChoiceType.FillInTable;
+		else
+			choiceType = SPFieldChoiceChoiceType.DropDownButton;
+		return choiceType;
 	}
 
 	private _setValueOnDropDownPlusFillIn(value): void {
@@ -53,16 +91,6 @@ export class FormFieldWrapperSPFieldChoice extends FormFieldWrapper {
 		return value;
 	}
 
-	private _getValueFromDropDown(): string {
-		let value;
-		if (this.fieldElement.querySelector("table[id$=FillInTable]")) {
-			value = this._getValuesFromDropDownPlusFillIn();
-		} else {
-			value = this.getValue();
-		}
-		return value;
-	}
-
 	private _getValuesFromDropDownPlusFillIn(): string {
 		let inputRadios = this.fieldElement.querySelectorAll("input[type='radio']");
 		let checkedRadio = Array.prototype.slice.call(inputRadios)
@@ -76,5 +104,12 @@ export class FormFieldWrapperSPFieldChoice extends FormFieldWrapper {
 		let selectedElement = this.fieldElement.querySelector(selector + "[title]");
 		let fieldValue = (<HTMLInputElement>selectedElement).value;
 		return fieldValue;
+	}
+	public addEventListener(type: string, callback: (ev: Event) => any): void {
+		super.addEventListener(type, callback);
+		if (this.fieldElement.querySelector("table[id$=FillInTable]")) {
+			this.fieldElement.querySelector("span.ms-RadioText").addEventListener("click", callback);
+			this.fieldElement.querySelector("input[type=text]").addEventListener("click", callback);
+		}
 	}
 }
