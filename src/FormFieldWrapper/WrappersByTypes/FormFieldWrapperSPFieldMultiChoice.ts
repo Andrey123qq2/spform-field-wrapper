76724530d59b73015e1d24ce6f3036b10641adbb
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-10 10:48:51 +0200

    feat: FormFieldWrapperSPFieldMultiChoice - added methods enable and disable; refactoring - moved getting input options from methods to private field

--- src/FormFieldWrapper/WrappersByTypes/FormFieldWrapperSPFieldMultiChoice.ts
+++ src/FormFieldWrapper/WrappersByTypes/FormFieldWrapperSPFieldMultiChoice.ts
@@ -1,25 +1,36 @@
 ﻿import { FormFieldWrapper } from "../FormFieldWrapper";
 
 export class FormFieldWrapperSPFieldMultiChoice extends FormFieldWrapper {
+	private _inputOptions: Array<HTMLInputElement>;
 	constructor(fieldTitle: string) {
 		super(fieldTitle);
+		if (!window.location.href.match(/DispForm\.aspx/i))
+			this._inputOptions = Array.prototype.slice.call(this.fieldElement.querySelectorAll("input[type='checkbox']"));
 	}
 
 	public get value(): Array<string> {
 		if (window.location.href.match(/DispForm\.aspx/i))
 			return this.getValueDispForm().split(/;\s?/);
 		var optionsValues = [];
-		let inputOptions = this.fieldElement.querySelectorAll("input[type='checkbox']");
-		optionsValues = Array.prototype.slice.call(inputOptions)
+		optionsValues = this._inputOptions
 			.filter(o => o.checked)
 			.map(o => o.nextElementSibling.textContent);
 		return optionsValues;
 	}
 
 	public set value(value) {
-		let inputOptions = this.fieldElement.querySelectorAll("input[type='checkbox']");
-		Array.prototype.slice.call(inputOptions)
+		this._inputOptions
 			.forEach(o => o.checked = Array.contains(value, o.nextElementSibling.textContent));
+	}
+
+	public enable(): void {
+		this._inputOptions
+			.forEach(i => i.removeAttribute("disabled"));
+	}
+
+	public disable(): void {
+		this._inputOptions
+			.forEach(i => i.setAttribute("disabled", "true"));
 	}
 
 	public toString(): string {
